5708f5eef443ff8824da613d6a673f175187ad80
WiFaCha <willbodet@gmail.com>
2024-12-18 13:11:51 +0100

    Update ProjectDialog.tsx

--- src/components/projects/ProjectDialog.tsx
+++ src/components/projects/ProjectDialog.tsx
@@ -4,6 +4,7 @@ import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -176,10 +177,15 @@ export function ProjectDialog({ project, onSubmit, trigger, open, onOpenChange }
               control={form.control}
               name="monthly_once"
               render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Une fois par mois</FormLabel>
+                <FormItem className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+                  <div className="space-y-0.5">
+                    <FormLabel>Chantier à faire une fois par mois</FormLabel>
+                  </div>
                   <FormControl>
-                    <Input type="checkbox" {...field} />
+                    <Switch
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />
                   </FormControl>
                 </FormItem>
               )}
